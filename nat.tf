f7c77023d51d50dc1245462e555d2532b84f23c2
pdoodipala <pdoodipala2@gmail.com>
2023-08-28 23:31:37 +0530

    updated nat for eip name

--- nat.tf
+++ nat.tf
@@ -1,5 +1,13 @@
 resource "aws_eip" "eip" {
   vpc = true
+
+  tags = {
+    Name = local.eip
+  }
+}
+
+locals {
+  eip="eip-terraform"
 }
 
 resource "aws_nat_gateway" "nat" {
@@ -7,8 +15,13 @@ resource "aws_nat_gateway" "nat" {
   subnet_id     = aws_subnet.private_subnets[0].id
 
   tags = {
-    Name = nat
+    Name = local.nat
   }
 
   depends_on = [aws_internet_gateway.gw]
 }
+
+locals {
+  nat="nat-terraform"
+}
+
