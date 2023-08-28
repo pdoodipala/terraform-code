2d596e5807eeacd64f54ce661e648094c9f859cf
pdoodipala <pdoodipala2@gmail.com>
2023-08-29 01:18:24 +0530

    updated

--- eks.tf
+++ eks.tf
@@ -94,7 +94,10 @@ resource "aws_eks_fargate_profile" "kube-system" {
   fargate_profile_name   = "kube-system"
   pod_execution_role_arn = aws_iam_role.eks-fargate-profile.arn
 
-  subnet_ids = ["aws_subnet.private_subnets[0].id", "aws_subnet.private_subnets[1].id"]  # Specify your subnet IDs
+  subnet_ids = [
+    aws_subnet.private_subnets[0].id, 
+    aws_subnet.private_subnets[1].id
+    ]  # Specify your subnet IDs
 
   selector {
     namespace = "kube-system"
