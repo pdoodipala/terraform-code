3fa944a38277eb8f6cc8331b3d9249aca26bbd15
pdoodipala <pdoodipala2@gmail.com>
2023-08-28 23:23:39 +0530

    updated

---
+++ nat.tf
@@ -0,0 +1,18 @@
+resource "aws_eip" "eip" {
+  vpc = true
+
+  tags = {
+    Name = eip
+  }
+}
+
+resource "aws_nat_gateway" "nat" {
+  allocation_id = aws_eip.eip.id
+  subnet_id     = aws_subnet.private_subnets[0].id
+
+  tags = {
+    Name = nat
+  }
+
+  depends_on = [aws_internet_gateway.gw]
+}
