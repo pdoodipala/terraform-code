7c19fa93d175338fe98ee2b9aa8c849331eaf61a
pdoodipala <pdoodipala2@gmail.com>
2023-08-28 23:37:39 +0530

    updated route table for nat

--- rt.tf
+++ rt.tf
@@ -16,3 +16,22 @@ resource "aws_route_table_association" "public_subnet_asso" {
  subnet_id      = element(aws_subnet.public_subnets[*].id, count.index)
  route_table_id = aws_route_table.second_rt.id
 }
+
+resource "aws_route_table" "third_rt" {
+ vpc_id = aws_vpc.main.id
+ 
+ route {
+   cidr_block = "0.0.0.0/0"
+   nat_gateway_id = aws_nat_gateway.nat.id
+ }
+ 
+ tags = {
+   Name = "3rd Route Table"
+ }
+}
+
+resource "aws_route_table_association" "private_subnet_asso" {
+ count = length(var.private_subnet_cidrs)
+ subnet_id      = element(aws_subnet.private_subnets[*].id, count.index)
+ route_table_id = aws_route_table.third_rt.id
+}
