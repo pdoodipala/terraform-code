e676a2aba421fb3e751388b34eb84c24587527f0
pdoodipala <pdoodipala2@gmail.com>
2023-08-29 00:15:55 +0530

    updated

--- provider.tf
+++ provider.tf
@@ -10,6 +10,6 @@ terraform {
 # Configure the AWS Provider
 provider "aws" {
   region = "eu-central-1"
-  access_key = "AKIATVGY6G6PDO4OWIUQ"
-  secret_key = "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"
+  access_key = "AKIATVGY6G6PJRUUEPP5"
+  secret_key = "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"
 }
